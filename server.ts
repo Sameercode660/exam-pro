9361a95a875f20acea209e68b1b5b8845c8e0eab
Mohd Sameer <mesh789736@gmail.com>
2025-07-24 20:39:15 +0530

    deployment test1

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import prisma from "@/utils/prisma";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
