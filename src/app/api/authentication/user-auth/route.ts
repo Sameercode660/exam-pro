575413f12b4ac19fa9166618429256d08b206483
Mohd Sameer <mesh789736@gmail.com>
2025-07-30 21:53:15 +0530

    added nodejs environment in apis

--- src/app/api/authentication/user-auth/route.ts
+++ src/app/api/authentication/user-auth/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { NextRequest, NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
 import prisma from '@/utils/prisma';
--- src/app/api/authentication/user/login/route.ts
+++ src/app/api/authentication/user/login/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/utils/prisma";
 import jwt from "jsonwebtoken";
