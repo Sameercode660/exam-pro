2aae12a6271e63289c6fc45e04c84a1082086b5f
Mohd Sameer <mesh789736@gmail.com>
2025-08-14 14:03:26 +0530

    added server iso date

--- src/app/api/date-time/route.ts
+++ src/app/api/date-time/route.ts
@@ -1,5 +1,5 @@
-import {NextResponse} from 'next/server'
+import { NextResponse } from "next/server";
 
 export async function GET() {
-    return NextResponse.json({date: new Date()})
+  return NextResponse.json({ date: new Date().toISOString() }); 
 }
